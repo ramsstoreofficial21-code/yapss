b4b0ea70639a1f611544a6b4987285be7d5c4777
ramsstoreofficial21-code <ramsstoreofficial21@gmail.com>
2025-12-09 22:06:08 +0700

    Create react.js

---
+++ api/react.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
+
+  const { postLink, reactions } = req.body
+
+  if (!postLink || !reactions) {
+    return res.status(400).json({ message: 'Masukkan link post dan reaksi' })
+  }
+
+  try {
+    const url = 'https://foreign-marna-sithaunarathnapromax-9a005c2e.koyeb.app/api/channel/react-to-post'
+    const requestData = { post_link: postLink, reacts: reactions }
+    const headers = {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer <TOKEN-MU>'
+    }
+
+    const response = await axios.post(url, requestData, { headers })
+    res.status(200).json(response.data)
+
+  } catch (error) {
+    console.error(error)
+    if (error.response) {
+      res.status(error.response.status).json({ message: error.response.data.message || 'Terjadi kesalahan API' })
+    } else {
+      res.status(500).json({ message: 'Terjadi kesalahan server' })
+    }
+  }
+}
