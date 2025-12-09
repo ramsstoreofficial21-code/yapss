c4dc6a39c2be255f4645d56aca4fe7724aed41f8
ramsstoreofficial21-code <ramsstoreofficial21@gmail.com>
2025-12-09 22:25:55 +0700

    Update react.js

--- api/react.js
+++ api/react.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+
 export default async function handler(req, res) {
   try {
     if (req.method !== 'POST') {
@@ -13,17 +14,17 @@ export default async function handler(req, res) {
     const url = 'https://foreign-marna-sithaunarathnapromax-9a005c2e.koyeb.app/api/channel/react-to-post'
     const requestData = { post_link: postLink, reacts: reactions }
     const headers = {
-      'Accept': 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5MzgyZDFhMTE0YWI3MTE5ZmNhNTdjZiIsImlhdCI6MTc2NTI4OTI0MiwiZXhwIjoxNzY1ODk0MDQyfQ.PyblreikWf2_fcPwRfrM_w-_VZmSlvk1vQtrrOuNFBo'
+  'Accept': 'application/json, text/plain, */*',
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5MzgyZDFhMTE0YWI3MTE5ZmNhNTdjZiIsImlhdCI6MTc2NTI4OTI0MiwiZXhwIjoxNzY1ODk0MDQyfQ.PyblreikWf2_fcPwRfrM_w-_VZmSlvk1vQtrrOuNFBo'
     }
 
     const response = await axios.post(url, requestData, { headers })
+
     return res.status(200).json(response.data)
 
   } catch (error) {
     console.error(error)
-    // selalu kirim JSON
     const message = error?.response?.data?.message || error.message || 'Terjadi kesalahan server'
     return res.status(500).json({ message })
   }
